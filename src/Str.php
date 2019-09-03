7a0e0a09498968a792f3ffe2f77e8c6a8ac66617
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-03 10:58:37 +0200

    Add Str::camel()
    Converts a string to camel case.

--- src/Str.php
+++ src/Str.php
@@ -7,4 +7,19 @@ namespace ipl\Stdlib;
  */
 class Str
 {
+    /**
+     * Convert the given string to camel case
+     *
+     * The given string may be delimited by the following characters: '_' (underscore), '-' (dash), ' ' (space).
+     *
+     * @param string $subject
+     *
+     * @return string
+     */
+    public static function camel($subject)
+    {
+        $normalized = str_replace(['-', '_'], ' ', $subject);
+
+        return lcfirst(str_replace(' ', '', ucwords(strtolower($normalized))));
+    }
 }
