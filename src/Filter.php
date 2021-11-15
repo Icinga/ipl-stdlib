bf75f7c11607159279ed5c4a8e918bfa7c8979f0
Yonas Habteab <yonas.habteab@netways.de>
2021-11-15 15:01:54 +0100

    `Filter`: Implement `hasValue()` & `matchHasValue()` helper methods

--- src/Filter.php
+++ src/Filter.php
@@ -11,6 +11,8 @@ use ipl\Stdlib\Filter\Condition;
 use ipl\Stdlib\Filter\Equal;
 use ipl\Stdlib\Filter\GreaterThan;
 use ipl\Stdlib\Filter\GreaterThanOrEqual;
+use ipl\Stdlib\Filter\HasNotValue;
+use ipl\Stdlib\Filter\HasValue;
 use ipl\Stdlib\Filter\LessThan;
 use ipl\Stdlib\Filter\LessThanOrEqual;
 use ipl\Stdlib\Filter\None;
@@ -141,6 +143,45 @@ class Filter
         }
 
         return true;
+    }
+
+    /**
+     * Create a rule that applies rows with a column having any values, i.e. matches
+     * all rows where this column has been set
+     *
+     * Value is optional, as it is not used anyway when executing the query, rather
+     * it is only being populated in the FilterEditor.
+     *
+     * @param string      $column
+     * @param string|null $value
+     *
+     * @return HasValue
+     */
+    public static function hasValue($column, $value = '')
+    {
+        return new HasValue($column, $value);
+    }
+
+    /**
+     * Get whether the given rule's column contains any values
+     *
+     * @param HasValue|HasNotValue $rule
+     * @param object $row
+     *
+     * @return bool
+     */
+    protected function matchHasValue($rule, $row)
+    {
+        if (! $rule instanceof HasValue && ! $rule instanceof HasNotValue) {
+            throw new InvalidArgumentException(sprintf(
+                'Rule must be of type %s or %s, got %s instead',
+                HasValue::class,
+                HasNotValue::class,
+                get_php_type($rule)
+            ));
+        }
+
+        return ! empty($this->extractValue($rule->getColumn(), $row));
     }
 
     /**
@@ -408,6 +449,8 @@ class Filter
                 return $this->matchNone($rule, $row);
             case $rule instanceof Unequal:
                 return $this->matchUnequal($rule, $row);
+            case $rule instanceof HasValue:
+                return $this->matchHasValue($rule, $row);
             default:
                 throw new InvalidArgumentException(sprintf(
                     'Unable to match filter. Rule type %s is unknown',
