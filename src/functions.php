be710cdf137232dcc04286936a1c437c9ae087f9
Johannes Meyer <johannes.meyer@icinga.com>
2025-09-05 13:53:17 +0200

    Introduce function `resolve_attribute`

--- src/functions.php
+++ src/functions.php
@@ -2,9 +2,14 @@
 
 namespace ipl\Stdlib;
 
+use Attribute;
 use Generator;
 use InvalidArgumentException;
+use ipl\Stdlib\Contract\MethodAttribute;
+use ipl\Stdlib\Contract\PropertyAttribute;
 use IteratorIterator;
+use ReflectionAttribute;
+use ReflectionClass;
 use Traversable;
 use stdClass;
 
@@ -125,4 +130,72 @@ function yield_groups(Traversable $traversable, callable $groupBy): Generator
     }
 
     yield $criterion => $group;
+}
+
+/**
+ * Resolve an attribute on an object
+ *
+ * This function will resolve and apply the attribute on the given object. Depending on the attribute's target,
+ * the attribute needs to implement the appropriate interface:
+ *
+ * - {@see PropertyAttribute} for properties
+ * - {@see MethodAttribute} for methods
+ *
+ * Supported attribute flags:
+ * - {@see Attribute::TARGET_PROPERTY}
+ * - {@see Attribute::TARGET_METHOD}
+ * - {@see Attribute::IS_REPEATABLE}
+ *
+ * @param class-string $attributeClass The attribute class to resolve. Must be an {@see Attribute}
+ * @param object $object The object to resolve the attribute on
+ * @param mixed ...$args Optional arguments to pass to the attribute's methods
+ *
+ * @return void
+ *
+ * @throws InvalidArgumentException If the given class is not a valid attribute
+ */
+function resolve_attribute(string $attributeClass, object $object, mixed &...$args): void
+{
+    $attrRef = new ReflectionClass($attributeClass);
+    $attrAttributes = $attrRef->getAttributes(Attribute::class);
+    if (empty($attrAttributes)) {
+        throw new InvalidArgumentException(sprintf('Class %s is not an attribute', $attributeClass));
+    }
+
+    $attr = $attrAttributes[0]->newInstance();
+    $objectRef = new ReflectionClass($object);
+
+    if ($attr->flags & Attribute::TARGET_PROPERTY) {
+        if (! $attrRef->implementsInterface(PropertyAttribute::class)) {
+            throw new InvalidArgumentException(sprintf(
+                'Class %s does not implement %s',
+                $attributeClass,
+                PropertyAttribute::class
+            ));
+        }
+
+        foreach ($objectRef->getProperties() as $property) {
+            $attributes = $property->getAttributes($attributeClass, ReflectionAttribute::IS_INSTANCEOF);
+            foreach ($attributes as $attribute) {
+                $attribute->newInstance()->applyToProperty($property, $object, ...$args);
+            }
+        }
+    }
+
+    if ($attr->flags & Attribute::TARGET_METHOD) {
+        if (! $attrRef->implementsInterface(MethodAttribute::class)) {
+            throw new InvalidArgumentException(sprintf(
+                'Class %s does not implement %s',
+                $attributeClass,
+                MethodAttribute::class
+            ));
+        }
+
+        foreach ($objectRef->getMethods() as $method) {
+            $attributes = $method->getAttributes($attributeClass, ReflectionAttribute::IS_INSTANCEOF);
+            foreach ($attributes as $attribute) {
+                $attribute->newInstance()->applyToMethod($method, $object, ...$args);
+            }
+        }
+    }
 }
