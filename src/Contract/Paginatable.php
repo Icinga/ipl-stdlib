748894b69e8cd0335eeff076354b6a031f4c65d5
Jan Schuppik <jan.schuppik@icinga.com>
2026-03-04 15:17:10 +0100

    `Paginatable`: Add strict type declarations
    This change requires adjustments to the following consumers:
    
    ipl-sql
        - src/Cursor.php
        - src/LimitOffset.php
    
    PHPDoc adjusted: nullable shorthand, union type ordering, compact format.

--- src/Contract/Paginatable.php
+++ src/Contract/Paginatable.php
@@ -11,46 +11,46 @@ interface Paginatable extends Countable
      *
      * @return bool
      */
-    public function hasLimit();
+    public function hasLimit(): bool;
 
     /**
      * Get the limit
      *
-     * @return int|null
+     * @return ?int
      */
-    public function getLimit();
+    public function getLimit(): ?int;
 
     /**
      * Set the limit
      *
-     * @param int|null $limit Maximum number of items to return. If you want to disable the limit,
-     *                        it is best practice to use null or a negative value
+     * @param ?int $limit Maximum number of items to return. If you want to disable the limit,
+     *   it is best practice to use null or a negative value
      *
      * @return $this
      */
-    public function limit($limit);
+    public function limit(?int $limit): static;
 
     /**
      * Get whether an offset is set
      *
      * @return bool
      */
-    public function hasOffset();
+    public function hasOffset(): bool;
 
     /**
      * Get the offset
      *
-     * @return int|null
+     * @return ?int
      */
-    public function getOffset();
+    public function getOffset(): ?int;
 
     /**
      * Set the offset
      *
-     * @param int|null $offset Start result set after this many rows. If you want to disable the offset,
-     *                         it is best practice to use null or a negative value
+     * @param ?int $offset Start result set after this many rows. If you want to disable the offset,
+     *   it is best practice to use null or a negative value
      *
      * @return $this
      */
-    public function offset($offset);
+    public function offset(?int $offset): static;
 }
